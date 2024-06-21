5c9bae1894bbf566be75ec3f9f04863abd448e97
Melvin Cheah <cheahhongjam@gmail.com>
2024-06-21 19:43:09 +0800

    updated package.json darkmode

--- shareme_frontend/src/components/Login.jsx
+++ shareme_frontend/src/components/Login.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { GoogleLogin } from '@react-oauth/google';
 import { useNavigate } from 'react-router-dom';
-import { FcGoogle } from 'react-icons/fc';
 import shareVideo from '../assets/share.mp4';
 import logo from '../assets/logo.png';
 import { jwtDecode } from 'jwt-decode';
